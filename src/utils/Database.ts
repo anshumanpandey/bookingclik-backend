8868a7a18fee1d4ba11ae38c50707291f19c3b4c
leo <leo_9_t@hotmail.com>
2020-04-12 07:11:55 -0400

    FIX on DB script

--- src/utils/Database.ts
+++ src/utils/Database.ts
@@ -1,8 +1,12 @@
 import { Sequelize } from "sequelize"
 
 // Option 1: Passing a connection URI
-export const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
+let sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
 
 // Option 2: Passing parameters separately (other dialects)
-// export const sequelize = new Sequelize(`mysql://rightcar_db:b239avp@151.236.32.180:3306/rightcar_db`)
+if (process.env.NODE_ENV === 'production') {
+    sequelize = new Sequelize(`mysql://rightcar_db:b239avp@151.236.32.180:3306/rightcar_db`)
+} 
+
+export { sequelize };
 
